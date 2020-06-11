206af3e6ec54cc640341db5abc8c2f0e14dc511b
Carlos Mostek <carlos.mostek@auth0.com>
2020-06-11 16:29:26 -0500

    Install hook should now create the API

--- server/routes/hooks.js
+++ server/routes/hooks.js
@@ -19,10 +19,19 @@ export default () => {
   hooks.delete('/on-uninstall', (req, res) => {
     logger.debug('Uninstall running version 0.0.1 ...');
     req.auth0.clients.delete({ client_id: config('AUTH0_CLIENT_ID') })
-      .then(() => {
-        logger.debug(`Deleted client: ${config('AUTH0_CLIENT_ID')}`);
-        res.sendStatus(204);
-      })
+      .then(() => req.auth0.resourceServers.delete({ identifier: config('EXTENSION_AUDIENCE') })
+        .then(() => {
+          logger.debug(`Deleted client: ${config('AUTH0_CLIENT_ID')}`);
+          logger.debug(`Deleted API: ${config('EXTENSION_AUDIENCE')}`);
+          res.sendStatus(204);
+        })
+        .catch((err) => {
+          logger.debug(`Error deleting API: ${config('EXTENSION_AUDIENCE')}`);
+          logger.error(err);
+
+          // Even if deleting fails, we need to be able to uninstall the extension.
+          res.sendStatus(204);
+        }))
       .catch((err) => {
         logger.debug(`Error deleting client: ${config('AUTH0_CLIENT_ID')}`);
         logger.error(err);
@@ -41,8 +50,36 @@ export default () => {
   hooks.post('/on-install', (req, res) => {
     console.log('carlos, checking console for install set to post');
     logger.info('Install running...');
+    // TODO: update the client here to set the appropriate grants, should only have client credentials
+    // TODO: create a rule that will block any user-based tokens for this API
+    req.auth0.resourceServers.create({
+      identifier: config('EXTENSION_AUDIENCE'),
+      name: 'idp-redirector-api',
+      scopes: [
+        {
+          value: 'update:patterns',
+          description: 'Update the whitelist patterns'
+        },
+        {
+          value: 'read:patterns',
+          description: 'Read the whitelist patterns'
+        }
+      ]
+    })
+      .then(() => {
+        logger.debug(`Created API: ${config('EXTENSION_AUDIENCE')}`);
+        return res.sendStatus(204);
+      })
+      .catch((err) => {
+        logger.debug(`Error creating API: ${config('EXTENSION_AUDIENCE')}`);
+        logger.error(err);
 
-    res.sendStatus(204);
+        // Even if deleting fails, we need to be able to uninstall the extension.
+        res.status(500).json({
+          error: 'failed_install',
+          error_description: 'Could not create the API'
+        });
+      });
   });
 
   return hooks;

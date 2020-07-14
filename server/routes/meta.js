339857f8843de14af46bf7f973293750b740c59c
zulfiqar <zulfiqar@auth0.com>
2020-07-14 12:20:22 +0500

    debug update model

--- server/routes/meta.js
+++ server/routes/meta.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const metadata = require("../../webtask.json");
+const axios = require("axios").default;
 
 module.exports = () => {
   const api = new express.Router();
-  api.get("/", (req, res) => {
-    res.status(200).send(metadata);
+  api.get("/", async (req, res) => {
+    const dynamicMetadata = await axios.get(metadata.xxMetadata);
+
+    res.status(200).send(dynamicMetadata);
   });
 
   return api;

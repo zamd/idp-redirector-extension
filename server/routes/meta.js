097c142d90cbdc5e8fba4190ab2b26de0692fc67
zulfiqar <zulfiqar@auth0.com>
2020-07-14 16:50:50 +0500

    implement update model with github release

--- server/routes/meta.js
+++ server/routes/meta.js
@@ -1,9 +1,24 @@
 const express = require("express");
 const metadata = require("../../webtask.json");
+const axios = require("axios");
+const semver = require("semver");
+const logger = require("../lib/logger");
 
 module.exports = () => {
   const api = new express.Router();
-  api.get("/", (req, res) => {
+  api.get("/", async (req, res) => {
+    const { repository } = metadata;
+    const latestReleaseUrl = repository
+      .replace("github.com", "api.github.com/repos")
+      .concat("/releases/latest")
+      .trim();
+    try {
+      const response = await axios.get(latestReleaseUrl);
+      const { tag_name: latestRelease } = response.data;
+      metadata.version = semver.parse(latestRelease).version.toString();
+    } catch (error) {
+      logger.verbose("Failed to get release info from github", error);
+    }
     res.status(200).send(metadata);
   });
 
---
+++ tests/server/meta.tests.js
@@ -0,0 +1,63 @@
+const nock = require("nock");
+const chai = require("chai");
+const request = require("supertest");
+const { describe, it, beforeEach } = require("mocha");
+const express = require("express");
+const proxyquire = require("proxyquire").noCallThru();
+
+const expect = chai.expect;
+
+const meta = proxyquire("../../server/routes/meta", {
+  "../../webtask.json": {
+    version: "1.0.0",
+    repository: "https://github.com/auth0-extension/idp-redirector-extension"
+  }
+});
+
+describe("#idp-redirector/meta", () => {
+  const app = express();
+  app.use("/meta", meta());
+
+  describe("GET /meta", () => {
+    beforeEach(() => {
+      nock("https://api.gethub.com")
+        .get("/repos/auth0-extension/idp-redirector-extension/releases/latest")
+        .reply();
+    });
+    it("returns version from webtask.json metadata", done => {
+      const expectedVersion = "1.0.0";
+      request(app)
+        .get("/meta")
+        .expect(200)
+        .end((err, res) => {
+          if (err) return done(err);
+
+          expect(res.body.version).to.equal(expectedVersion);
+
+          done();
+        });
+    });
+
+    it("returns version from github release tag", done => {
+      const expectedVersion = "3.0.0";
+
+      nock("https://api.gethub.com")
+        .get("/repos/auth0-extension/idp-redirector-extension/releases/latest")
+        .reply(200, {
+          id: 123456,
+          tag_name: "v" + expectedVersion
+        });
+
+      request(app)
+        .get("/meta")
+        .expect(200)
+        .end((err, res) => {
+          if (err) return done(err);
+
+          expect(res.body.version).to.equal(expectedVersion);
+
+          done();
+        });
+    });
+  });
+});
